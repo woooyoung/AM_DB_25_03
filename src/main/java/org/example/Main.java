53b12ce8f570ac58b16ebb41df821167c06fd3fa
woooyoung <axdsw121@gmail.com>
2025-03-28 13:48:19 +0900

    글 작성 시 DB에 INSERT 되도록

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,10 @@
 package org.example;
 
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -34,6 +38,53 @@ public class Main {
 
                 lastArticleId++;
                 System.out.println(article);
+                System.out.println(id + "번 글이 작성되었습니다");
+
+                /// ///////////////////////////////////////////
+                Connection conn = null;
+                PreparedStatement pstmt = null;
+
+                try {
+                    Class.forName("org.mariadb.jdbc.Driver");
+                    String url = "jdbc:mariadb://127.0.0.1:3306/AM_DB_25_03?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
+                    conn = DriverManager.getConnection(url, "root", "");
+                    System.out.println("연결 성공!");
+
+                    String sql = "insert into article";
+                    sql += " set regDate = now(),";
+                    sql += "updateDate = now(),";
+                    sql += "title = '" + title + "',";
+                    sql += "`body` = '" + body + "';";
+
+                    System.out.println(sql);
+
+                    pstmt = conn.prepareStatement(sql);
+
+                    int affectedRows = pstmt.executeUpdate();
+
+                    System.out.println("affected rows: " + affectedRows);
+
+                } catch (ClassNotFoundException e) {
+                    System.out.println("드라이버 로딩 실패" + e);
+                } catch (SQLException e) {
+                    System.out.println("에러 : " + e);
+                } finally {
+                    try {
+                        if (conn != null && !conn.isClosed()) {
+                            conn.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        if (pstmt != null && !pstmt.isClosed()) {
+                            pstmt.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+
             } else if (cmd.equals("article list")) {
                 System.out.println("==목록==");
                 if (articles.size() == 0) {
