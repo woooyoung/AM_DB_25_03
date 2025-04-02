c46fb9bd0145264a0dc4c374f50f07274224ad49
woooyoung <axdsw121@gmail.com>
2025-04-02 10:31:27 +0900

    회원가입 기능 구현

--- DB.sql
+++ DB.sql
@@ -2,12 +2,21 @@ drop database if exists AM_DB_25_03;
 create database AM_DB_25_03;
 use AM_DB_25_03;
 
-create table article(
-                        id int(10) unsigned not null primary key auto_increment,
-                        regDate datetime not null,
-                        updateDate datetime not null,
-                        title char(100) not null,
-                        `body` text not null
+create table article (
+                         id int(10) unsigned not null primary key auto_increment,
+                         regDate datetime not null,
+                         updateDate datetime not null,
+                         title char(100) not null,
+                         `body` text not null
+);
+
+create table `member` (
+                          id int(10) unsigned not null primary key auto_increment,
+                          regDate datetime not null,
+                          updateDate datetime not null,
+                          loginId char(30) not null,
+                          loginPw CHAR(200) not null,
+                          `name` char(100) not null
 );
 
 INSERT INTO article
@@ -28,9 +37,39 @@ updateDate = NOW(),
 title = '제목3',
 `body` = '내용3';
 
+insert into `member`
+set regDate = now(),
+updateDate = now(),
+loginId = 'test1',
+loginPw = 'test1',
+`name` = '김철수';
+
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'test2',
+loginPw = 'test2',
+`name` = '홍길동';
+
+
+
+######################################################################
+
 select *
 from article
 order by id desc;
+
+SELECT *
+FROM `member`;
+
+select count(*) > 0
+from `member`
+where loginId = 'test2';
+
+select 1 + 1;
+select 1 >= 1;
+
+select COUNT(*) > 0 from `member` where loginId = 'test3';
 
 select now();
 
@@ -47,6 +86,13 @@ set regDate = now(),
 updateDate = now(),
 title = concat('제목',substring(RAND() * 1000 from 1 for 2)),
 `body` = concat('내용',substring(RAND() * 1000 from 1 for 2));
+
+insert into `member`
+set regDate = now(),
+updateDate = now(),
+loginId = concat('loginId ',substring(RAND() * 1000 from 1 for 2)),
+loginPw = concat('loginPw ',substring(RAND() * 1000 from 1 for 2)),
+`name` = CONCAT('name ',SUBSTRING(RAND() * 1000 FROM 1 FOR 2));
 
 update article
 set updateDate = now(),
